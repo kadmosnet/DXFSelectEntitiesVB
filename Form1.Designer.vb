25df9019336c521f601a1f3c87485e7562232c53
Daniele Alberti <kadmos@kadmos.com>
2023-10-26 12:08:48 +0200

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,274 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim DxfDocument1 As DXFReaderNET.DxfDocument = New DXFReaderNET.DxfDocument()
+        Dim AciColor1 As DXFReaderNET.AciColor = New DXFReaderNET.AciColor()
+        Dim RasterVariables1 As DXFReaderNET.Objects.RasterVariables = New DXFReaderNET.Objects.RasterVariables()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.dxfReaderNETControl1 = New DXFReaderNET.DXFReaderNETControl()
+        Me.statusStrip1 = New System.Windows.Forms.StatusStrip()
+        Me.toolStripStatusLabel1 = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.saveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
+        Me.openFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.menuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.fileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.newDrawingToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.toolStripSeparator3 = New System.Windows.Forms.ToolStripSeparator()
+        Me.loadDXFFileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.saveDXFFileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.toolStripSeparator2 = New System.Windows.Forms.ToolStripSeparator()
+        Me.toolStripSeparator1 = New System.Windows.Forms.ToolStripSeparator()
+        Me.exitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.viewToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.zoomExtentsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.zoomWindowToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.toolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.aboutDXFReaderNETComponentToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripMenuItem2 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SelectEntityWithAMouseClickToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SelectEntitiesWithRectangleToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.statusStrip1.SuspendLayout()
+        Me.menuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'dxfReaderNETControl1
+        '
+        Me.dxfReaderNETControl1.AutoSize = True
+        Me.dxfReaderNETControl1.ContinuousHighlight = False
+        Me.dxfReaderNETControl1.CustomCursor = DXFReaderNET.CustomCursorType.None
+        Me.dxfReaderNETControl1.DistributionCode = ""
+        DxfDocument1.ActiveLayout = "Model"
+        DxfDocument1.Comments = Nothing
+        AciColor1.Index = CType(256, Short)
+        AciColor1.IsByBlock = False
+        AciColor1.IsByLayer = True
+        AciColor1.UseTrueColor = False
+        DxfDocument1.CurrentColor = AciColor1
+        DxfDocument1.CurrentElevation = 0R
+        DxfDocument1.CurrentLayer = "0"
+        DxfDocument1.CurrentLineTypeName = "ByLayer"
+        DxfDocument1.CurrentLineTypeScale = 1.0R
+        DxfDocument1.CurrentTextSize = 2.5R
+        DxfDocument1.CurrentTextStyle = "STANDARD"
+        DxfDocument1.CurrentThickness = 0R
+        DxfDocument1.DateCreated = New Date(2020, 4, 24, 8, 56, 22, 681)
+        DxfDocument1.DateLastEdited = New Date(2020, 4, 24, 8, 56, 22, 681)
+        DxfDocument1.Handle = "0"
+        DxfDocument1.IsBinary = False
+        DxfDocument1.Modified = False
+        DxfDocument1.Name = Nothing
+        DxfDocument1.Owner = Nothing
+        RasterVariables1.DisplayFrame = True
+        RasterVariables1.DisplayQuality = DXFReaderNET.Objects.ImageDisplayQuality.High
+        RasterVariables1.Handle = "2D"
+        RasterVariables1.Owner = Nothing
+        RasterVariables1.Units = DXFReaderNET.ImageUnits.Unitless
+        DxfDocument1.RasterVariables = RasterVariables1
+        DxfDocument1.ThumbnailImage = Nothing
+        DxfDocument1.TotalEditTime = New Date(2020, 4, 24, 0, 0, 0, 0)
+        DxfDocument1.TotalEditTimeDays = 0
+        DxfDocument1.UserTimer = New Date(2020, 4, 24, 0, 0, 0, 0)
+        DxfDocument1.UserTimerDays = 0
+        Me.dxfReaderNETControl1.DXF = DxfDocument1
+        Me.dxfReaderNETControl1.FileName = Nothing
+        Me.dxfReaderNETControl1.HighlightGrabPoints = False
+        Me.dxfReaderNETControl1.HighlightGrabPointsColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(163, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.dxfReaderNETControl1.HighlightMarkerColor2 = System.Drawing.Color.Magenta
+        Me.dxfReaderNETControl1.HighlightNotContinuous = False
+        Me.dxfReaderNETControl1.Location = New System.Drawing.Point(91, 34)
+        Me.dxfReaderNETControl1.Name = "dxfReaderNETControl1"
+        Me.dxfReaderNETControl1.ShowAxes = False
+        Me.dxfReaderNETControl1.ShowGrid = True
+        Me.dxfReaderNETControl1.ShowGridRuler = False
+        Me.dxfReaderNETControl1.Size = New System.Drawing.Size(818, 467)
+        Me.dxfReaderNETControl1.TabIndex = 11
+        '
+        'statusStrip1
+        '
+        Me.statusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.toolStripStatusLabel1})
+        Me.statusStrip1.Location = New System.Drawing.Point(0, 546)
+        Me.statusStrip1.Name = "statusStrip1"
+        Me.statusStrip1.Size = New System.Drawing.Size(1059, 22)
+        Me.statusStrip1.TabIndex = 12
+        Me.statusStrip1.Text = "statusStrip1"
+        '
+        'toolStripStatusLabel1
+        '
+        Me.toolStripStatusLabel1.Name = "toolStripStatusLabel1"
+        Me.toolStripStatusLabel1.Size = New System.Drawing.Size(118, 17)
+        Me.toolStripStatusLabel1.Text = "toolStripStatusLabel1"
+        '
+        'openFileDialog1
+        '
+        Me.openFileDialog1.FileName = "openFileDialog1"
+        '
+        'menuStrip1
+        '
+        Me.menuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.fileToolStripMenuItem, Me.ToolStripMenuItem2, Me.viewToolStripMenuItem, Me.toolStripMenuItem1})
+        Me.menuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.menuStrip1.Name = "menuStrip1"
+        Me.menuStrip1.Size = New System.Drawing.Size(1059, 24)
+        Me.menuStrip1.TabIndex = 13
+        Me.menuStrip1.Text = "menuStrip1"
+        '
+        'fileToolStripMenuItem
+        '
+        Me.fileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.newDrawingToolStripMenuItem, Me.toolStripSeparator3, Me.loadDXFFileToolStripMenuItem, Me.saveDXFFileToolStripMenuItem, Me.toolStripSeparator2, Me.toolStripSeparator1, Me.exitToolStripMenuItem})
+        Me.fileToolStripMenuItem.Name = "fileToolStripMenuItem"
+        Me.fileToolStripMenuItem.Size = New System.Drawing.Size(37, 20)
+        Me.fileToolStripMenuItem.Text = "File"
+        '
+        'newDrawingToolStripMenuItem
+        '
+        Me.newDrawingToolStripMenuItem.Name = "newDrawingToolStripMenuItem"
+        Me.newDrawingToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.newDrawingToolStripMenuItem.Text = "New drawing"
+        '
+        'toolStripSeparator3
+        '
+        Me.toolStripSeparator3.Name = "toolStripSeparator3"
+        Me.toolStripSeparator3.Size = New System.Drawing.Size(149, 6)
+        '
+        'loadDXFFileToolStripMenuItem
+        '
+        Me.loadDXFFileToolStripMenuItem.Name = "loadDXFFileToolStripMenuItem"
+        Me.loadDXFFileToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.loadDXFFileToolStripMenuItem.Text = "Load DXF file..."
+        '
+        'saveDXFFileToolStripMenuItem
+        '
+        Me.saveDXFFileToolStripMenuItem.Name = "saveDXFFileToolStripMenuItem"
+        Me.saveDXFFileToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.saveDXFFileToolStripMenuItem.Text = "Save DXF file..."
+        '
+        'toolStripSeparator2
+        '
+        Me.toolStripSeparator2.Name = "toolStripSeparator2"
+        Me.toolStripSeparator2.Size = New System.Drawing.Size(149, 6)
+        '
+        'toolStripSeparator1
+        '
+        Me.toolStripSeparator1.Name = "toolStripSeparator1"
+        Me.toolStripSeparator1.Size = New System.Drawing.Size(149, 6)
+        '
+        'exitToolStripMenuItem
+        '
+        Me.exitToolStripMenuItem.Name = "exitToolStripMenuItem"
+        Me.exitToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.exitToolStripMenuItem.Text = "Exit"
+        '
+        'viewToolStripMenuItem
+        '
+        Me.viewToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.zoomExtentsToolStripMenuItem, Me.zoomWindowToolStripMenuItem})
+        Me.viewToolStripMenuItem.Name = "viewToolStripMenuItem"
+        Me.viewToolStripMenuItem.Size = New System.Drawing.Size(44, 20)
+        Me.viewToolStripMenuItem.Text = "View"
+        '
+        'zoomExtentsToolStripMenuItem
+        '
+        Me.zoomExtentsToolStripMenuItem.Name = "zoomExtentsToolStripMenuItem"
+        Me.zoomExtentsToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.zoomExtentsToolStripMenuItem.Text = "Zoom extents"
+        '
+        'zoomWindowToolStripMenuItem
+        '
+        Me.zoomWindowToolStripMenuItem.Name = "zoomWindowToolStripMenuItem"
+        Me.zoomWindowToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.zoomWindowToolStripMenuItem.Text = "Zoom window"
+        '
+        'toolStripMenuItem1
+        '
+        Me.toolStripMenuItem1.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.aboutDXFReaderNETComponentToolStripMenuItem})
+        Me.toolStripMenuItem1.Name = "toolStripMenuItem1"
+        Me.toolStripMenuItem1.Size = New System.Drawing.Size(24, 20)
+        Me.toolStripMenuItem1.Text = "?"
+        '
+        'aboutDXFReaderNETComponentToolStripMenuItem
+        '
+        Me.aboutDXFReaderNETComponentToolStripMenuItem.Name = "aboutDXFReaderNETComponentToolStripMenuItem"
+        Me.aboutDXFReaderNETComponentToolStripMenuItem.Size = New System.Drawing.Size(267, 22)
+        Me.aboutDXFReaderNETComponentToolStripMenuItem.Text = "About DXFReader.NET Component..."
+        '
+        'ToolStripMenuItem2
+        '
+        Me.ToolStripMenuItem2.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.SelectEntityWithAMouseClickToolStripMenuItem, Me.SelectEntitiesWithRectangleToolStripMenuItem})
+        Me.ToolStripMenuItem2.Name = "ToolStripMenuItem2"
+        Me.ToolStripMenuItem2.Size = New System.Drawing.Size(57, 20)
+        Me.ToolStripMenuItem2.Text = "Modify"
+        '
+        'SelectEntityWithAMouseClickToolStripMenuItem
+        '
+        Me.SelectEntityWithAMouseClickToolStripMenuItem.Name = "SelectEntityWithAMouseClickToolStripMenuItem"
+        Me.SelectEntityWithAMouseClickToolStripMenuItem.Size = New System.Drawing.Size(239, 22)
+        Me.SelectEntityWithAMouseClickToolStripMenuItem.Text = "Select entity with a mouse click"
+        '
+        'SelectEntitiesWithRectangleToolStripMenuItem
+        '
+        Me.SelectEntitiesWithRectangleToolStripMenuItem.Name = "SelectEntitiesWithRectangleToolStripMenuItem"
+        Me.SelectEntitiesWithRectangleToolStripMenuItem.Size = New System.Drawing.Size(239, 22)
+        Me.SelectEntitiesWithRectangleToolStripMenuItem.Text = "Select entities with rectangle"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1059, 568)
+        Me.Controls.Add(Me.menuStrip1)
+        Me.Controls.Add(Me.dxfReaderNETControl1)
+        Me.Controls.Add(Me.statusStrip1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "DXFReader.NET - Template"
+        Me.statusStrip1.ResumeLayout(False)
+        Me.statusStrip1.PerformLayout()
+        Me.menuStrip1.ResumeLayout(False)
+        Me.menuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Private WithEvents dxfReaderNETControl1 As DXFReaderNET.DXFReaderNETControl
+    Private WithEvents statusStrip1 As StatusStrip
+    Private WithEvents toolStripStatusLabel1 As ToolStripStatusLabel
+    Private WithEvents saveFileDialog1 As SaveFileDialog
+    Private WithEvents openFileDialog1 As OpenFileDialog
+    Private WithEvents menuStrip1 As MenuStrip
+    Private WithEvents fileToolStripMenuItem As ToolStripMenuItem
+    Private WithEvents newDrawingToolStripMenuItem As ToolStripMenuItem
+    Private WithEvents toolStripSeparator3 As ToolStripSeparator
+    Private WithEvents loadDXFFileToolStripMenuItem As ToolStripMenuItem
+    Private WithEvents saveDXFFileToolStripMenuItem As ToolStripMenuItem
+    Private WithEvents toolStripSeparator2 As ToolStripSeparator
+    Private WithEvents toolStripSeparator1 As ToolStripSeparator
+    Private WithEvents exitToolStripMenuItem As ToolStripMenuItem
+    Private WithEvents viewToolStripMenuItem As ToolStripMenuItem
+    Private WithEvents zoomExtentsToolStripMenuItem As ToolStripMenuItem
+    Private WithEvents zoomWindowToolStripMenuItem As ToolStripMenuItem
+    Private WithEvents toolStripMenuItem1 As ToolStripMenuItem
+    Private WithEvents aboutDXFReaderNETComponentToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem2 As ToolStripMenuItem
+    Friend WithEvents SelectEntityWithAMouseClickToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SelectEntitiesWithRectangleToolStripMenuItem As ToolStripMenuItem
+End Class
---
+++ Form1.vb
@@ -0,0 +1,226 @@
+﻿
+Imports DXFReaderNET
+Imports DXFReaderNET.Entities
+
+Public Class Form1
+
+    Friend Enum FunctionsEnum
+        None
+        ZoomWindow1
+        ZoomWindow2
+        GetEntity
+        GetEntities1
+        GetEntities2
+    End Enum
+
+    Private CurrentFunction As FunctionsEnum = FunctionsEnum.None
+    Private p1 As Vector2 = Vector2.Zero
+    Private p2 As Vector2 = Vector2.Zero
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.Width = Screen.PrimaryScreen.Bounds.Width - 40
+        Me.Height = Screen.PrimaryScreen.Bounds.Height - 80
+        Me.Left = 20
+        Me.Top = 20
+
+        dxfReaderNETControl1.NewDrawing()
+        dxfReaderNETControl1.CustomCursor = CustomCursorType.CrossHair
+        toolStripStatusLabel1.Text = ""
+        dxfReaderNETControl1.Dock = DockStyle.Fill
+    End Sub
+
+    Private Sub newDrawingToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles newDrawingToolStripMenuItem.Click
+        dxfReaderNETControl1.NewDrawing()
+    End Sub
+
+    Private Sub loadDXFFileToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles loadDXFFileToolStripMenuItem.Click
+
+        openFileDialog1.DefaultExt = "DXF"
+        openFileDialog1.Filter = "DXF|*.dxf"
+        openFileDialog1.FileName = ""
+
+        If openFileDialog1.ShowDialog() = DialogResult.OK Then
+            dxfReaderNETControl1.ReadDXF(openFileDialog1.FileName)
+        End If
+
+    End Sub
+
+    Private Sub saveDXFFileToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles saveDXFFileToolStripMenuItem.Click
+
+        saveFileDialog1.DefaultExt = "dxf"
+        saveFileDialog1.Filter = "DXF|*.dxf"
+        saveFileDialog1.FileName = "drawing.dxf"
+
+        If saveFileDialog1.ShowDialog() = DialogResult.OK Then
+            dxfReaderNETControl1.WriteDXF(saveFileDialog1.FileName)
+        End If
+    End Sub
+
+    Private Sub exitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles exitToolStripMenuItem.Click
+        End
+    End Sub
+
+    Private Sub zoomExtentsToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles zoomExtentsToolStripMenuItem.Click
+        dxfReaderNETControl1.ZoomExtents()
+    End Sub
+
+    Private Sub zoomWindowToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles zoomWindowToolStripMenuItem.Click
+        CurrentFunction = FunctionsEnum.ZoomWindow1
+        toolStripStatusLabel1.Text = "Select start point of the window"
+    End Sub
+
+    Private Sub dxfReaderNETControl1_MouseMove(sender As Object, e As MouseEventArgs) Handles dxfReaderNETControl1.MouseMove
+        Select Case CurrentFunction
+            Case FunctionsEnum.ZoomWindow2, FunctionsEnum.GetEntities2
+
+                dxfReaderNETControl1.ShowRubberBandBox(p1, dxfReaderNETControl1.CurrentWCSpoint)
+        End Select
+    End Sub
+
+    Private Sub dxfReaderNETControl1_MouseUp(sender As Object, e As MouseEventArgs) Handles dxfReaderNETControl1.MouseUp
+        If e.Button = MouseButtons.Left Then
+
+            Select Case CurrentFunction
+                Case FunctionsEnum.ZoomWindow2
+                    CurrentFunction = FunctionsEnum.None
+                    dxfReaderNETControl1.CustomCursor = CustomCursorType.CrossHair
+                    toolStripStatusLabel1.Text = ""
+                    dxfReaderNETControl1.ZoomWindow(p1, dxfReaderNETControl1.CurrentWCSpoint)
+                Case FunctionsEnum.ZoomWindow1
+                    CurrentFunction = FunctionsEnum.ZoomWindow2
+                    toolStripStatusLabel1.Text = "Select end point of the window"
+                    p1 = dxfReaderNETControl1.CurrentWCSpoint
+                Case FunctionsEnum.GetEntity
+                    toolStripStatusLabel1.Text = ""
+                    CurrentFunction = FunctionsEnum.None
+                    Dim ent As EntityObject = dxfReaderNETControl1.GetEntity(dxfReaderNETControl1.CurrentWCSpoint)
+
+                    If ent IsNot Nothing Then
+                        toolStripStatusLabel1.Text = EntityInfo(dxfReaderNETControl1, ent)
+
+                        If Not dxfReaderNETControl1.DXF.SelectedEntities.Contains(ent) Then
+                            dxfReaderNETControl1.DXF.SelectedEntities.Add(ent)
+                            dxfReaderNETControl1.HighLight(ent)
+                        End If
+                    Else
+                        toolStripStatusLabel1.Text = "No entity found"
+                    End If
+
+                Case FunctionsEnum.GetEntities2
+                    CurrentFunction = FunctionsEnum.None
+                    toolStripStatusLabel1.Text = ""
+                    p2 = dxfReaderNETControl1.CurrentWCSpoint
+
+                    For Each entity As EntityObject In dxfReaderNETControl1.GetEntities(p1, p2)
+
+                        If Not dxfReaderNETControl1.DXF.SelectedEntities.Contains(entity) Then
+                            dxfReaderNETControl1.DXF.SelectedEntities.Add(entity)
+                        End If
+                    Next
+
+                    toolStripStatusLabel1.Text = "Selected " & dxfReaderNETControl1.DXF.SelectedEntities.Count.ToString() & " entities"
+                    dxfReaderNETControl1.HighLight(dxfReaderNETControl1.DXF.SelectedEntities)
+                Case FunctionsEnum.GetEntities1
+                    CurrentFunction = FunctionsEnum.GetEntities2
+                    toolStripStatusLabel1.Text = "Select end point of selection rectangle"
+                    p1 = dxfReaderNETControl1.CurrentWCSpoint
+            End Select
+        End If
+    End Sub
+
+    Private Sub aboutDXFReaderNETComponentToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles aboutDXFReaderNETComponentToolStripMenuItem.Click
+        dxfReaderNETControl1.About()
+    End Sub
+
+    Private Sub SelectEntityWithAMouseClickToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SelectEntityWithAMouseClickToolStripMenuItem.Click
+        CurrentFunction = FunctionsEnum.GetEntity
+        dxfReaderNETControl1.CustomCursor = CustomCursorType.CrossHairSquare
+        toolStripStatusLabel1.Text = "Select entity"
+    End Sub
+
+    Private Sub SelectEntitiesWithRectangleToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SelectEntitiesWithRectangleToolStripMenuItem.Click
+        CurrentFunction = FunctionsEnum.GetEntities1
+
+        toolStripStatusLabel1.Text = "Select start point of selection rectangle"
+    End Sub
+    Private Shared Function EntityInfo(ByVal myDXF As DXFReaderNETControl, ByVal ent As EntityObject) As String
+        Dim s As String = ""
+        Dim colorE As String = ent.Color.ToString()
+        If colorE = "ByLayer" Then colorE = myDXF.DXF.Layers(ent.Layer.Name).Color.ToString()
+
+        Select Case colorE
+            Case "1"
+                colorE = "Red"
+            Case "2"
+                colorE = "Yellow"
+            Case "3"
+                colorE = "Green"
+            Case "4"
+                colorE = "Cyan"
+            Case "5"
+                colorE = "Blue"
+            Case "6"
+                colorE = "Magenta"
+            Case "7"
+                colorE = "White"
+        End Select
+
+        Dim ltE As String = ent.Linetype.Name
+        If ltE = "ByLayer" Then ltE = myDXF.DXF.Layers(ent.Layer.Name).Linetype.Name
+        s = ent.Type.ToString() & " " + ent.Handle & " - Color: " & colorE & " - Layer: " + ent.Layer.Name & " - Linetype: " & ltE
+
+        Select Case ent.Type
+            Case EntityType.Line
+                s += " - Length: " & myDXF.DXF.ToFormattedUnit((CType(ent, Line)).Lenght)
+                s += " - Start point: (" & myDXF.DXF.ToFormattedUnit((CType(ent, Line)).StartPoint.X) & ";" + myDXF.DXF.ToFormattedUnit((CType(ent, Line)).StartPoint.Y) & ")"
+                s += " - End point: (" & myDXF.DXF.ToFormattedUnit((CType(ent, Line)).EndPoint.X) & ";" + myDXF.DXF.ToFormattedUnit((CType(ent, Line)).EndPoint.Y) & ")"
+            Case EntityType.XLine
+                s += " - Origin: (" & myDXF.DXF.ToFormattedUnit((CType(ent, XLine)).Origin.X) & ";" + myDXF.DXF.ToFormattedUnit((CType(ent, XLine)).Origin.Y) & ")"
+                s += " - Direction: (" & myDXF.DXF.ToFormattedUnit((CType(ent, XLine)).Direction.X) & ";" + myDXF.DXF.ToFormattedUnit((CType(ent, XLine)).Direction.Y) & ")"
+            Case EntityType.Ray
+                s += " - Origin: (" & myDXF.DXF.ToFormattedUnit((CType(ent, Ray)).Origin.X) & ";" + myDXF.DXF.ToFormattedUnit((CType(ent, Ray)).Origin.Y) & ")"
+                s += " - Direction: (" & myDXF.DXF.ToFormattedUnit((CType(ent, Ray)).Direction.X) & ";" + myDXF.DXF.ToFormattedUnit((CType(ent, Ray)).Direction.Y) & ")"
+            Case EntityType.Arc
+                s += " - Length: " & myDXF.DXF.ToFormattedUnit((CType(ent, Arc)).Lenght)
+                s += " - Start point: (" & myDXF.DXF.ToFormattedUnit((CType(ent, Arc)).StartPoint.X) & ";" + myDXF.DXF.ToFormattedUnit((CType(ent, Arc)).StartPoint.Y) & ")"
+                s += " - End point: (" & myDXF.DXF.ToFormattedUnit((CType(ent, Arc)).EndPoint.X) & ";" + myDXF.DXF.ToFormattedUnit((CType(ent, Arc)).EndPoint.Y) & ")"
+                s += " - Start angle: " & myDXF.DXF.ToFormattedAngle((CType(ent, Arc)).StartAngle * MathHelper.DegToRad)
+                s += " - End angle: " & myDXF.DXF.ToFormattedAngle((CType(ent, Arc)).EndAngle * MathHelper.DegToRad)
+                s += " - Radius: " & myDXF.DXF.ToFormattedUnit((CType(ent, Arc)).Radius)
+            Case EntityType.Circle
+                s += " - Length: " & myDXF.DXF.ToFormattedUnit((CType(ent, Circle)).Lenght)
+                s += " - Area: " & myDXF.DXF.ToFormattedUnit((CType(ent, Circle)).Area)
+                s += " - Center point: (" & myDXF.DXF.ToFormattedUnit((CType(ent, Circle)).Center.X) & ";" + myDXF.DXF.ToFormattedUnit((CType(ent, Circle)).Center.Y) & ")"
+                s += " - Radius: " & myDXF.DXF.ToFormattedUnit((CType(ent, Circle)).Radius)
+            Case EntityType.Ellipse
+                s += " - Length: " & myDXF.DXF.ToFormattedUnit((CType(ent, Ellipse)).Lenght)
+                s += " - Area: " & myDXF.DXF.ToFormattedUnit((CType(ent, Ellipse)).Area)
+                s += " - Center point: (" & myDXF.DXF.ToFormattedUnit((CType(ent, Ellipse)).Center.X) & ";" + myDXF.DXF.ToFormattedUnit((CType(ent, Ellipse)).Center.Y) & ")"
+                s += " - MajorAxis: " & myDXF.DXF.ToFormattedUnit((CType(ent, Ellipse)).MajorAxis)
+                s += " - MinorAxis: " & myDXF.DXF.ToFormattedUnit((CType(ent, Ellipse)).MinorAxis)
+                s += " - Rotation: " & myDXF.DXF.ToFormattedAngle((CType(ent, Ellipse)).Rotation * MathHelper.DegToRad)
+            Case EntityType.LightWeightPolyline
+                s += " - Length: " & myDXF.DXF.ToFormattedUnit((CType(ent, LwPolyline)).Lenght)
+                If (CType(ent, LwPolyline)).IsClosed Then s += " - Area: " & myDXF.DXF.ToFormattedUnit((CType(ent, LwPolyline)).Area)
+                s += " - Vertexes #: " & (CType(ent, LwPolyline)).Vertexes.Count.ToString()
+            Case EntityType.Polyline
+                s += " - Length: " & myDXF.DXF.ToFormattedUnit((CType(ent, Polyline)).Lenght)
+                If (CType(ent, Polyline)).IsClosed Then s += " - Area: " & myDXF.DXF.ToFormattedUnit((CType(ent, Polyline)).Area)
+                s += " - Vertexes #: " & (CType(ent, Polyline)).Vertexes.Count.ToString()
+            Case EntityType.Insert
+                s += " - Insert point: (" & myDXF.DXF.ToFormattedUnit((CType(ent, Insert)).Position.X) & ";" + myDXF.DXF.ToFormattedUnit((CType(ent, Insert)).Position.Y) & ")"
+                s += " - Block name: " & (CType(ent, Insert)).Block.Name
+        End Select
+
+        For Each _group As DXFReaderNET.Objects.Group In myDXF.DXF.Groups
+
+            If _group.Entities.Contains(ent) Then
+                s += " Group: " & _group.Name
+                Exit For
+            End If
+        Next
+
+        Return s
+    End Function
+End Class
